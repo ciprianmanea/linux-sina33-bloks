8e1f4f36ac11f785394b6af226a8382e8a39898a
Manuel Traut <manut@linutronix.de>
2016-02-18 07:45:50 +0100

    debug prints
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-udisks --mount /dev/sdf1
+udisks --mount /dev/sdg1
 sudo cp /scratch/linux-bloks/arch/arm/boot/dts/sun8i-a33-sinlinx-sina33.dtb /media/root/boot/
 sudo cp /scratch/linux-bloks/arch/arm/boot/zImage /media/root/boot/
+sudo INSTALL_MOD_PATH=/media/root/ ./build.sh modules_install
 sync
-udisks --unmount /dev/sdf1
+udisks --unmount /dev/sdg1
